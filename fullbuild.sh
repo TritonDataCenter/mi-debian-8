2062bd59446e24442ffaca5d1809e472e7864d94
Christopher Horrell <christopher@joyent.com>
2014-03-12 20:44:57 +0000

    Syncing, not copying

--- fullbuild.sh
+++ fullbuild.sh
@@ -19,7 +19,7 @@ cd ..
 
 includes=config/chroot_local-includes/
 sdcvmtools=sdc-vmtools/src/linux
-echo "Copying etc, lib, and usr directories to ${includes}..."
+echo "Syncing etc, lib, and usr directories to ${includes}..."
 # Using rsync to ensure deleted files from sdc-vmtools repo are removed
 rsync -aq --delete --exclude=install-tools.sh ./${sdcvmtools}/ ${includes}/
 
